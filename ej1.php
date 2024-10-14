9507f4ec475cd545ab0b360bada9acbe7d3755b3
jazaro2425 <jazaro@alumnatflorida.es>
2024-10-14 10:38:50 +0200

    Add files via upload

---
+++ ej1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ejercicio1</title>
+</head>
+<body>
+    <h1>Ejercicio 1</h1>
+    
+    <?php
+        //INTRODUCE AQUÍ TU CÓDIGO
+    
+        
+    ?>
+    
+    <form method="POST" action="">
+        <label for="kilos">PAQUETES:</label>
+        <input type="number" id="paquetes" name="paquetes" value="">
+        <label for="tamano">TAMAÑO:</label>
+        <input type="number" id="tamano" name="tamano" value="">
+        <label for="peso">PESO:</label>
+        <input type="number" id="peso" name="peso" value="">
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+</html>
